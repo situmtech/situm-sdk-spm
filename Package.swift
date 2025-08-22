ced7677ba7df766de39425a490d7b79f176b3db9
Situmops <sistemas@situm.es>
2025-08-22 09:44:29 +0000

    Create release version branch 3.34.4

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SitumSDK",
-            url: "https://repo.situm.com:443/artifactory/libs-release-local/iOS/SitumSDK/3.34.3/SitumSDK.xcframework.zip",
-            checksum: "5ce13f5f153edd41844f240e349ad1907c0c99f669f1c89c97f9de55d295ae79"
+            url: "https://repo.situm.com:443/artifactory/libs-release-local/iOS/SitumSDK/3.34.4/SitumSDK.xcframework.zip",
+            checksum: "25dd7000920c313ee21e1e6af18d2db0160bed9c33c95b6318098e0f9eba1eb3"
         ),
         .target(
              name: "SitumSDKTarget",
