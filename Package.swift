b3a46a56e7b204cb62c10b95dc1558c5eac06f23
fsvilas <fernando.sanchez@situm.com>
2026-07-27 10:24:02 +0200

    Changes to use dinamyc sdk

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "SitumSDK",
-    platforms: [.iOS(.v12)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "SitumSDK",
@@ -13,32 +13,31 @@ let package = Package(
         ),
     ],
     dependencies: [
+        .package(url: "https://github.com/ZipArchive/ZipArchive.git", from: "2.6.0"),
     ],
     targets: [
         .binaryTarget(
             name: "SitumSDK",
-            url: "https://repo.situm.com:443/artifactory/libs-release-local/iOS/SitumSDK/3.40.0/SitumSDK.xcframework.zip",
+            url: "https://repo.situm.com:443/artifactory/libs-release-local/iOS/SitumSDK/3.41.0/SitumSDK.xcframework.noprotobuf.zip",
             checksum: "07dd85f1e2140d68e6f5f4eb7455ca9e3e5b6e92442e32e4682ebfef115d0d1c"
         ),
         .target(
-             name: "SitumSDKTarget",
-             dependencies: ["SitumSDK"],
-             path: "Situm",
-             sources: ["SITEmpty.m"],
-             publicHeadersPath: "Headers",
-             linkerSettings: [
-               .linkedLibrary("c++"),
-               .linkedLibrary("z"),
-               .linkedFramework("CoreLocation"),
-               .linkedFramework("CoreMotion"),
-             ]
+              name: "SitumSDKTarget",
+              dependencies: [
+                "SitumSDK",
+                .product(name: "ZipArchive", package: "ZipArchive"),
+              ],
+              path: "Situm",
+              sources: ["SITEmpty.m"],
+              publicHeadersPath: "Headers",
+              linkerSettings: [
+                .linkedLibrary("c++"),
+                .linkedLibrary("z"),
+                .linkedLibrary("iconv"),
+                .linkedFramework("CoreLocation"),
+                .linkedFramework("CoreMotion"),
+                .linkedFramework("Security"),
+              ]
         ),
     ]
 )
-
-
-/*
- * Make sure to manually add the following settings in the project using this package:
- *
- * Linker settings: Add `$(inherited) -ObjC` to `OTHER_LDFLAGS` in the build settings of your project.
- */
--- Readme.md
+++ Readme.md
@@ -3,16 +3,14 @@ Situm SDK is a Plug-and-play UX component that will allow you to bring wayfindin
 
 ## Adding the dependency
 
-`SitumSDK` is designed for Swift 5.8 and later. To depend on `SitumSDK`, either use Xcode to add the package dependency or add the following dependency to your Package.swift:
+`SitumSDK` is designed for Swift 5.8 and later and requires iOS 16 or later. To depend on `SitumSDK`, either use Xcode to add the package dependency or add the following dependency to your Package.swift:
 
 ```swift
-.package(url: "https://github.com/situmtech/situm-sdk-spm.git", .upToNextMinor(from: "3.19.0")),
+.package(url: "https://github.com/situmtech/situm-sdk-spm.git", .upToNextMinor(from: "3.41.0")),
 ```
 
-The version 3.19.0 is just and example. Choose properly the range on versions you want to rely on.
+The version 3.41.0 is just an example. Choose properly the range on versions you want to rely on.
 
-Afterwards make sure you manually add the following system libraries and settings in the target that is using this package:
-
- * Linker settings: Apply the `-ObjC` flag. In `Configuration`, under `Linking/Other Linker Flags`, add the flag `-ObjC`.
+The Swift Package declares ZipArchive 2.6.0 as an external dependency. The package links CoreLocation, CoreMotion, Security, libc++, libz, and libiconv.
 
  
