4d79695f8bfeb6fc7db41cddd72a48cb5d2c97d1
fsvilas <fernando.sanchez@situm.com>
2026-08-03 11:49:08 +0200

    Point to test dynamic sdk

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SitumSDK",
-            url: "https://repo.situm.com:443/artifactory/libs-release-local/iOS/SitumSDK/3.41.0/SitumSDK.xcframework.noprotobuf.zip",
-            checksum: "07dd85f1e2140d68e6f5f4eb7455ca9e3e5b6e92442e32e4682ebfef115d0d1c"
+            url: "https://repo.situm.es:443/artifactory/libs-release-local/iOS/SitumSDK/test_dynamyc/SitumSDK.xcframework.noprotobuf.zip",
+            checksum: "314ab1e3858ba10aac2df37ecd1e23377061e8be88a4b130e351f67a0ccf8ec6"
         ),
         .target(
               name: "SitumSDKTarget",
